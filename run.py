a8df28b717b9eb07e0a565335a39a408697a29bb
pallavipallewarp5 <pallavipallewar@gmail.com>
2023-04-19 12:35:58 +0000

    file path

--- run.py
+++ run.py
@@ -1,3 +1,14 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import datetime
+import time
+import pandas as pd
+import gazpacho
+
+
+""" Get real time data which is update every week now,from URL """
+
+URL = "https://opendata-geohive.hub.arcgis.com/datasets/d8eb52d56273413b84b0187a4e9117be_0.csv?outSR=%7B%22latestWkid%22%3A3857%2C%22wkid%22%3A102100%7D"
+
+
+
+ 
+
